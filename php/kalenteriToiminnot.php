718692c635dbba44f8af53bc1622bb72c2a4d291
janial <janial@metropolia.fi>
2020-04-10 13:42:36 +0300

    typo

--- php/kalenteriToiminnot.php
+++ php/kalenteriToiminnot.php
@@ -2,7 +2,7 @@
     // Hae käyttäjäntiedot userOlio-muuttujaan
     $userOlio = getUserData($_SESSION['user_ID'], $DBH);
     $user_ID = $userOlio->user_ID;
-    print_r($userOlio);
+    //print_r($userOlio);
 
     if(isset($_GET['prevDay'])){ // Vaihda edelliseen päivään
         $updatedDay = getPrevDay($_SESSION['currentDay']); // Korjaa päivä edeltäväksi ja tallenna tämän hetkiseksi päiväksi/viikoksi
